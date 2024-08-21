2eca254c6ba21ec57c4a20c588b8f06b4f3bc3f7
Scott <scotthoek@me.com>
2024-08-20 23:39:52 -0400

    implemented buildLoginUrl and getAccessToken for LinkedIn Auth

--- src/authControllers/linkedinAuthController.ts
+++ src/authControllers/linkedinAuthController.ts
@@ -1,23 +1,61 @@
-const axios = require('axios');
-
-
-
-
 import { oauthCredential, PageConnection, Platforms, Token } from "../types";
 import { AuthController } from "./authController";
 
+const axios = require('axios');
+
 export class LinkedInAuthController implements AuthController{
 
+    private credential: oauthCredential;
+
+    constructor(linkedInOauthCredential: oauthCredential) {
+      this.credential = linkedInOauthCredential;
+    }
+  
     buildLoginUrl(): string {
-        throw new Error("Method not implemented.");
-        //TODO: build login url
+
+        const authUrl = 'https://www.linkedin.com/oauth/v2/authorization';
+
+        const queryParams = new URLSearchParams({
+            client_id: this.credential.clientId,
+            redirect_uri: this.credential.redirectUri,
+            response_type: 'code',
+            scope: this.credential.scope || 'r_organization_social w_organization_social w_member_social r_basicprofile rw_organization_admin',
+          }).toString();
+        
+          return `${authUrl}?${queryParams}`;
 
     }
-    getAccessToken(code: string): Promise<Token> {
-        throw new Error("Method not implemented.");
-        //TODO: Get auth token
+
+    async getAccessToken(code: string): Promise<Token> {
+
+        const response = await axios.post(
+            "https://www.linkedin.com/oauth/v2/accessToken",
+            new URLSearchParams({
+                grant_type: 'authorization_code',
+                code: code,
+                redirect_uri: this.credential.redirectUri,
+                client_id: this.credential.clientId,
+                client_secret: this.credential.clientSecret,
+            }), {
+                headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+             });
+              
+        const { access_token, expires_in } = response.data;
+        
+        // Fetch user profile
+        const userResponse = await axios.get('https://api.linkedin.com/v2/me', {
+            headers: { Authorization: `Bearer ${access_token}` }
+        });
+        const user_id = userResponse.data.id;
+    
+        return {
+            token: access_token,
+            expiresIn: expires_in,
+            userId: user_id
+        }
 
     }
+
     fetchPages(token: Token): Promise<PageConnection[]> {
         throw new Error("Method not implemented.");
         //TODO: fetch pages
