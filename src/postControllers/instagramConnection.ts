8a54eed6e697419c21825b53930570363670af0b
Scott <scotthoek@me.com>
2024-08-21 22:03:03 -0400

    migrated code to create IG post from socialTable

--- src/postControllers/instagramConnection.ts
+++ src/postControllers/instagramConnection.ts
@@ -1,4 +1,5 @@
-import { Platforms, Post, PostStatus } from "../types";
+import axios from "axios";
+import { Platforms, Post, PostStatus, PostType } from "../types";
 import { PageConnection } from "./pageConnection";
 
 export class InstagramConnection implements PageConnection{
@@ -14,8 +15,49 @@ export class InstagramConnection implements PageConnection{
         this.pageId = pageId;
         this.name = name;
     }
-    createPost(post: Post): Promise<PostStatus> {
-        throw new Error("Method not implemented.");
+    async createPost(post: Post): Promise<PostStatus> {
+
+        let postUrl = "";
+
+        try {
+            if (post.type == PostType.Text && post.mediaUrl == null) {
+              throw new Error('Instagram does not support text-only posts');
+            } else if (post.type == PostType.Link && post.mediaUrl == null) {
+              throw new Error('Instagram does not support link-only posts');
+            } else if (post.type == PostType.Image || post.mediaUrl != null) {
+              const mediaResponse = await axios.post(`https://graph.facebook.com/v20.0/${this.pageId}/media`, {
+                image_url: post.mediaUrl,
+                caption: post.content,
+                access_token: this.accessToken,
+              });
+        
+              //console.log(`Image Uploaded: ID: ${mediaResponse.data.id}`, mediaResponse.data)
+        
+              const publishResponse = await axios.post(`https://graph.facebook.com/v20.0/${this.pageId}/media_publish`, {
+                creation_id: mediaResponse.data.id,
+                access_token: this.accessToken,
+              });
+        
+              postUrl = `https://www.instagram.com/p/${publishResponse.data.id}`;
+                  
+            }
+            return {
+                postUrl: postUrl,
+                success: true
+            };
+          } catch (err: any) {
+            if (err.response) {
+                return {
+                    error: err.response.data,
+                    success: false
+                };
+            } else {
+                return {
+                    error: err.message,
+                    success: false
+                };
+            }
+          }
     }
 
 }
